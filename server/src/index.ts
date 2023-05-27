2b2a6a93f4caed5772e7e6a18840e18a57014891
teslmik <teslmik@gmail.com>
2023-05-27 17:39:17 +0300

    implement server, model user & token

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI as string);
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
