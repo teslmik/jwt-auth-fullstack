dc22b856a2afd24682f9e035b0124654a33c30ca
teslmik <teslmik@gmail.com>
2023-05-27 18:09:22 +0300

    add user controller

---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from 'express';
+
+class UserController {
+  async registration(req: Request, res: Response, next: NextFunction) {
+    try {
+    } catch (error) { }
+  }
+
+  async login(req: Request, res: Response, next: NextFunction) {
+    try {
+    } catch (error) { }
+  }
+
+  async logout(req: Request, res: Response, next: NextFunction) {
+    try {
+    } catch (error) { }
+  }
+
+  async activate(req: Request, res: Response, next: NextFunction) {
+    try {
+    } catch (error) { }
+  }
+
+  async refresh(req: Request, res: Response, next: NextFunction) {
+    try {
+    } catch (error) { }
+  }
+
+  async getUsers(req: Request, res: Response, next: NextFunction) {
+    try {
+      res.json(['123', '456']);
+    } catch (error) { }
+  }
+}
+
+export { UserController };
--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
+import { UserController } from '../controllers/user.controller.js';
 
 const router = Router();
+const userController = new UserController();
 
-router.post('/registration');
-router.post('/login');
-router.post('/logout');
-router.get('/activate/:link');
-router.get('/refresh');
-router.get('/users');
+router.post('/registration', userController.registration);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/activate/:link', userController.activate);
+router.get('/refresh', userController.refresh);
+router.get('/users', userController.getUsers);
 
 export default router;
